137279653fd8d4a25801cbcc2acca999af2efdeb
Anton Stepanov <to@stepa.dev>
2021-02-12 23:13:13 +0300

    Added vapor-http package and deleted auto-generated test files

--- Sources/swift-http-framework/main.swift
+++ Sources/swift-http-framework/main.swift
@@ -1,1 +1,4 @@
-print("Hello, world!")
+// swift-http-frmework
+
+
+
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import swift_http_frameworkTests
-
-var tests = [XCTestCaseEntry]()
-tests += swift_http_frameworkTests.allTests()
-XCTMain(tests)
