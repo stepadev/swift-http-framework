e2076ae34d009c2109265ba123107bbb9cdd72ea
Anton Stepanov <to@stepa.dev>
2021-02-13 22:50:46 +0300

    Added func parseQueryParams and print request parametr

--- Sources/swift-http-framework/main.swift
+++ Sources/swift-http-framework/main.swift
@@ -3,17 +3,36 @@
 
 import HTTP
 
-let hostname = "127.0.0.1"
-let port: Int = 8080
-
-let res = HTTPResponse(body: "pong" as StaticString)
+//var res = HTTPResponse(body: "Hello")
 
 struct EchoResponder: HTTPServerResponder {
     func respond(to req: HTTPRequest, on worker: Worker) -> Future<HTTPResponse> {
+        
+        print("-------Start--------")
+        
+        func parseQueryParams(_ req: HTTPRequest) -> [String:String] {
+            var paramsDictionary = [String:String]()
+            guard let queryParams = req.url.query else { return paramsDictionary }
+            let params = queryParams.split(separator: "&")
+            for param in params {
+                let item = param.split(separator: "=")
+                if item.count == 2 { paramsDictionary[String(item[0])] = String(item[1]) }
+                else { paramsDictionary[String(item[0])] = "" }
+            }
+            return paramsDictionary
+        }
+
+        print(parseQueryParams(req))
+        
+        let name = parseQueryParams(req)["name"] ?? "Guest"
+        let res = HTTPResponse(body: "Hello, \(name)!")
+        
         return worker.eventLoop.newSucceededFuture(result: res)
     }
 }
 
+let hostname = "localhost"
+let port = 8000
 print("Server starting on http://\(hostname):\(port)")
 let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
 defer { try! group.syncShutdownGracefully() }
