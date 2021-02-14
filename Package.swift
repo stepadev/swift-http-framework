87af5507862fa18eda4f9927fca1da2450adf6f2
Anton Stepanov <to@stepa.dev>
2021-02-14 13:32:45 +0300

    Changed structure project, rename folders and edit Package file

--- Package.swift
+++ Package.swift
@@ -5,22 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "swift-http-framework",
+    products: [
+        .executable(name: "swift-http-framework", targets: ["Run"]),
+    ],
     dependencies: [
         //.package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(name: "HTTP", url: "https://github.com/vapor/http.git", from: "3.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swift-http-framework",
+            name: "Run",
+            dependencies: [
+                "Framework",
+                .product(name: "HTTP", package: "HTTP"),
+            ]),
+        .target(
+            name: "Framework",
             dependencies: [
                 //.product(name: "NIO", package: "swift-nio"),
                 //.product(name: "NIOHTTP1", package: "swift-nio"),
                 .product(name: "HTTP", package: "HTTP"),
             ]),
         .testTarget(
-            name: "swift-http-frameworkTests",
-            dependencies: ["swift-http-framework"]),
+            name: "FrameworkTests",
+            dependencies: ["Framework"]),
     ]
 )
