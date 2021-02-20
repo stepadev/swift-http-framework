2daaf6842c92b759deb9a7216bf8d27ae8145cc2
Anton Stepanov <to@stepa.dev>
2021-02-20 21:05:03 +0300

    Added methods withQueryParams(), withParsedBody() and cloneHTTPRequest() for create objects - modificated copies of requests

--- Sources/Framework/Http/Request.swift
+++ Sources/Framework/Http/Request.swift
@@ -3,18 +3,92 @@ import HTTP
 
 extension HTTPRequest {
     
-    private var queryParams : [String:String]? { return parseQueryParams() }
-    private var bodyParams : [String:String]? { return parseBodyParams() }
+    // Returns new HTTPRequest with given query params
+    public func withQueryParams(
+        _ params: [String:String],
+        _ method: HTTPMethod = .GET) -> HTTPRequest {
+        
+        guard params.count != 0 else { return self }
+        
+        // if isset body params
+        var json: Data?
+        if let bodyParams = getParsedBody() {
+            json = try? JSONEncoder().encode(bodyParams)
+        }
 
-    public func getQueryParams() -> [String:String]? { return queryParams }
-    public func getParsedBody() -> [String:String]? { return bodyParams }
+        // parse params and create string
+        var urlQuery = ""
+        for param in params {
+            if param.key != "" {
+                urlQuery += "\(param.key)=\(param.value)&"
+            }
+        }
+                
+        // create copy of request with query string
+        return cloneHTTPRequest(urlQuery, json, method)
+    }
     
-    private func parseQueryParams() -> [String:String]? {
+    // Returns new HTTPRequest with given body params
+    public func withParsedBody(
+        _ params: [String:String],
+        _ method: HTTPMethod = .GET) -> HTTPRequest {
+        
+        guard params.count != 0 else { return self }
+        
+        // if isset query params
+        var urlQuery = ""
+        if let queryParams = getQueryParams() {
+            for param in queryParams {
+                if param.key != "" {
+                    urlQuery += "\(param.key)=\(param.value)&"
+                }
+            }
+        }
+        
+        // create json encode data with given params
+        var json: Data?
+        if params.count > 0 && params.first?.key != "" {
+            json = try? JSONEncoder().encode(params)
+        }
+
+        // create copy of request with body data
+        return cloneHTTPRequest(urlQuery, json, method)
+    }
+    
+    private func cloneHTTPRequest(
+        _ queryString: String,
+        _ bodyParams: Data?,
+        _ method: HTTPMethod) -> HTTPRequest {
+        
+        var request = self
+    
+        // All internal copy of HTTPRequest's with body params will create with Content-Type: application/json
+        // For all copy of HTTPRequest's without body params header "Content-Type" will be removed
+        if request.headers.firstValue(name: .contentType) == nil && bodyParams != nil {
+            request.headers.add(name: "Content-Type", value: "application/json")
+        } else if request.headers.firstValue(name: .contentType) != nil && bodyParams == nil {
+            request.headers.remove(name: .contentType)
+        } else if request.headers.firstValue(name: .contentType) != nil && bodyParams != nil {
+            if request.headers.firstValue(name: .contentType) != "application/json" {
+                request.headers.replaceOrAdd(name: "Content-Type", value: "application/json")
+            }
+        }
+        
+        request.method = method
+        request.url = URL(string: (queryString.isEmpty) ? "/" : "/?\(queryString)")!
+        request.version = HTTPVersion(major: request.version.major, minor: request.version.minor)
+        request.headers = request.headers
+        request.body = ((bodyParams) != nil) ? HTTPBody(data: bodyParams!) : HTTPBody()
+        
+        return request
+    }
+    
+    public func getQueryParams() -> [String:String]? {
         guard let queryString = url.query else { return nil }
         return getDictionaryFromQuerySrtring(string: queryString)
     }
     
-    private func parseBodyParams() -> [String:String]? {
+    public func getParsedBody() -> [String:String]? {
         guard let contentType = contentType else { return nil }
         var dictionaryResult: [String:String] = [:]
         switch contentType {
@@ -27,7 +101,7 @@ extension HTTPRequest {
         case .urlEncodedForm:
             dictionaryResult = getDictionaryFromQuerySrtring(string: body.description)
         default:
-            print("Unsupported type")
+            print("Unsupported type:", contentType)
             return nil
         }
         return dictionaryResult
@@ -39,7 +113,7 @@ extension HTTPRequest {
         for param in params {
             let item = param.split(separator: "=")
             if item.count == 2 { dictionaryResult[String(item[0])] = String(item[1]) }
-            else { dictionaryResult[String(item[0])] = "" }
+            else if !item.isEmpty { dictionaryResult[String(item[0])] = "" }
         }
         return dictionaryResult
     }
